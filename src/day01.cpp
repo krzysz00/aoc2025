77248067690c6cc39f010f262059116be23c2780
Krzysztof Drewniak <krzysdrewniak@gmail.com>
2026-01-10 23:54:39 -0800

    Day 1, part 2

--- src/day01.cpp
+++ src/day01.cpp
@@ -75,10 +75,37 @@ int32_t solveA(const std::span<Entry> data) {
   return ret;
 }
 
+int32_t solveB(const std::span<Entry> data) {
+  int32_t state = 50;
+  int32_t ret = 0;
+  constexpr int32_t NUM_POS = 100;
+  for (const Entry &e : data) {
+    if (e.dir == Dir::L) {
+      bool startedAtZero = state == 0;
+      state -= e.turns;
+      if (state <= 0) {
+        // The division rounds-to-zero, so it won't count fact that we went
+        // negative in the first place, so we correct for that. However, if
+        // we're already at 0, we counted that in the iteration that got us
+        // there.
+        ret += (-state) / NUM_POS + !startedAtZero;
+      }
+    } else if (e.dir == Dir::R) {
+      state += e.turns;
+      ret += state / NUM_POS;
+    }
+
+    // Take a modulus to return us into a [0, 100) intermediate state.
+    state = ((state % NUM_POS) + NUM_POS) % NUM_POS;
+  }
+  return ret;
+}
+
 int main(int argc, char **argv) {
   std::string input = getInput(argc, argv);
   vector<Entry> parsed = parse(input);
   int32_t partA = solveA(parsed);
-  std::cout << "Part A: " << partA << "\n";
+  int32_t partB = solveB(parsed);
+  std::cout << "Part A: " << partA << "\n" << "Part B: " << partB << "\n";
   return 0;
 }
